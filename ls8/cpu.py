c14ef1392e4f26d35e26f7ca48f7efa8328920fb
ZaStack <stackzpersonal@gmail.com>
2020-07-20 16:08:19 -0700

    MVP Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,18 @@
 
 import sys
 
+LDI = 0b10000010
+HLT = 0b00000001
+PRN = 0b01000111
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -30,6 +36,11 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -62,4 +73,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if opcode == HLT:
+                self.running = False
+                self.pc += 1
+                sys.exit(0)
+            elif opcode == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif opcode == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'Nope. Who knows why.')
+                sys.exit(1)
